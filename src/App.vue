6fe2d7dac025ab77d4a60a7caaa719b870f2519b
Cyril Chan <kwunming0911@gmail.com>
2025-07-03 23:29:09 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import DynamicListFilter from "@/components/DynamicListFilter.vue";
+</script>
+
+<template>
+  <v-card>
+    <v-card-title>VUE playground</v-card-title>
+    <DynamicListFilter/>
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/DynamicListFilter.vue
@@ -0,0 +1,53 @@
+<script setup>
+import {ref, watch} from "vue";
+
+const users = [
+  {
+    id: '1',
+    name: 'Ian',
+    email: 'ian@gmail.com'
+  },
+  {
+    id: '2',
+    name: 'Cyril',
+    email: 'cyril@gmail.com'
+  },
+  {
+    id: '3',
+    name: 'Candy',
+    email: 'candy@gmail.com'
+  },
+  {
+    id: '4',
+    name: 'Sam',
+    email: 'sam@gmail.com'
+  },
+  {
+    id: '5',
+    name: 'Evans',
+    email: 'evans@gmail.com'
+  }
+];
+
+const searchQuery = ref('')
+
+const filteredList = ref(users)
+
+watch(searchQuery, (newValue) => {
+  filteredList.value = users.filter((user) => user.name.toLowerCase().includes(newValue.toLowerCase()))
+})
+
+</script>
+
+<template>
+  <input type="text" v-model="searchQuery"/>
+  <ul>
+    <li v-for="user in filteredList">
+      <span>{{ user.name }}</span>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
